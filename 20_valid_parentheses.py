ee7ed07d168701aba3b8be1baf96b36bd2ad1bd3
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2022-10-10 14:44:42 +0300

    20_valid_parentheses solved with cheating :(

---
+++ 20_valid_parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {
+            "(": ")",
+            "[": "]",
+            "{": "}"
+        }
+        
+        stack = []
+        
+        for char in s:
+            if char in brackets:
+                stack.append(brackets[char])
+            else:
+                if stack and stack[-1] == char:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+
+
+x = Solution()
+print(x.isValid('([)]'))
+        
