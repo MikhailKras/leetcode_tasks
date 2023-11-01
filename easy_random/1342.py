1bc70cd60b93f0ae0690d842b62c203ac84ddf1c
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-11-01 12:10:22 +0300

    feat: added easy ramdom problem 1342

---
+++ easy_random/1342.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        res = 0
+        while num != 0:
+            if num % 2 == 1:
+                num -= 1
+            else:
+                num //= 2
+            res += 1
+        return res
+
