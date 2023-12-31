5d8e5fefaa7b0bddb5647d003d9e828d263af8b3
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-12-31 22:14:55 +0300

    feat: added easy random problem 989

---
+++ easy_random/989.py
@@ -0,0 +1,8 @@
+import sys
+sys.set_int_max_str_digits(0)
+
+
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+       return list(map(int, list(str((int(''.join(map(str, num))) + k)))))
+
