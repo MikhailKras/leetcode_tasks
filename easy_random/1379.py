9e5a4aed005c913c9115b532ebaf4da5d02272c5
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2024-01-31 20:58:17 +0300

    feat: added easy random problem 1379

---
+++ easy_random/1379.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        stack = [cloned]
+        while stack:
+            cur = stack.pop()
+            if cur.val == target.val:
+                return cur
+            if cur.left:
+                stack.append(cur.left)
+            if cur.right:
+                stack.append(cur.right)
+        
