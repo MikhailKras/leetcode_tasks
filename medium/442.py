f2b0ac979ff16ab86555f6ff2cf0e60f359d88f5
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2024-03-06 19:32:59 +0300

    feat: added medium problem 442

---
+++ medium/442.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+        for num in nums:
+            n = abs(num)
+            if nums[n - 1] < 0:
+                res.append(n)
+            nums[n - 1] = -nums[n - 1]
+        return res
+        
