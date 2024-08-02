0dff63f8755f2a63ae52c4e2fef7286e69f70a28
mikhail <mikhailkrasovitskiy@gmail.com>
2024-08-02 21:11:29 +0300

    feat: added medium problem 1190

---
+++ medium/1190.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        res = ""
+
+        for char in s:
+            if char == "(":
+                stack.append(res)
+                res = ""
+            elif char == ")":
+                res = res[::-1]
+                res = stack.pop() + res
+            else:
+                res += char
+        
+        return res
+        
