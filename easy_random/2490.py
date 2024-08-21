5505e06be5b404fd8b717d20fce21fc20e65362b
mikhail <mikhailkrasovitskiy@gmail.com>
2024-08-21 22:12:26 +0300

    feat: added easy random problem 2490

---
+++ easy_random/2490.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        sentence = sentence.split()
+        for i in range(1, len(sentence)):
+            if sentence[i - 1][-1] != sentence[i][0]:
+                return False
+        
+        return sentence[0][0] == sentence[-1][-1]
+
