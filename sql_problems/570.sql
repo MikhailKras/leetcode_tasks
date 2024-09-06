6683e90e9320f6e12681f35a16c6cfd09fb544a2
mikhail <mikhailkrasovitskiy@gmail.com>
2024-09-06 22:58:58 +0300

    feat: added sql problem 570

---
+++ sql_problems/570.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT name
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(managerId) >= 5
+)
+
