fe9b775e55727ed0fba5407cd71f655181c63a24
mikhail <mikhailkrasovitskiy@gmail.com>
2024-09-24 19:06:56 +0300

    feat: added sql problem 1587

---
+++ sql_problems/1587.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT u.name,
+    SUM(t.amount) AS balance
+FROM Users u
+LEFT JOIN Transactions t
+ON t.account = u.account
+GROUP BY u.name
+HAVING SUM(t.amount) > 10000
+
