80cc3f7cff6ce98135e5890a07c71e2975533987
mikhail <mikhailkrasovitskiy@gmail.com>
2024-12-17 22:22:12 +0300

    feat: added easy random problem 1374

--- easy_random/1374.py
+++ easy_random/1374.py
@@ -1,7 +1,9 @@
 class Solution:
     def generateTheString(self, n: int) -> str:
+        if n == 1:
+            return 'a'
         if n % 2 == 0:
             return 'a' + (n - 1) * 'b'
         else:
             return 'a' + 'b' + (n - 2) * 'c'
-        
+         
