cfd19cdc9a44a351a65eee46b30e0c6e66b8f951
mikhail <mikhailkrasovitskiy@gmail.com>
2025-01-07 22:14:17 +0300

    feat: added easy random problem 3289

---
+++ easy_random/3289.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        ans = []
+        seen = set()
+        for x in nums: 
+            if x in seen:
+                ans.append(x)
+            else:
+                seen.add(x)
+        return ans 
+
